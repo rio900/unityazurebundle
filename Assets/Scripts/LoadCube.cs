a77af70b0aaa2a30a1d33758435fdc61454f13ef
Rio9000 <rio900@icloud.com>
2017-12-08 23:28:04 +0200

    Init

---
+++ Assets/Scripts/LoadCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadCube : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        //string uri = "file:///" + Application.dataPath + "/AssetBundles/mycube.hd";
+        string uri = "https://mytestbundle.blob.core.windows.net/windows/mycube.hd";
+        UnityWebRequest request = UnityWebRequest.GetAssetBundle(uri, 0);
+        yield return request.Send();
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+        GameObject cube = bundle.LoadAsset<GameObject>("CubeName");
+        Instantiate(cube);
+    }
+}
